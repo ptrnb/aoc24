1c387c6c4cf6fb7b5ad2993566ab7f05b39387f7
Peter Brown <>
2025-01-31 16:29:57 +1030

    Add day09

---
+++ 2024/rust/day09/src/bin/part2.rs
@@ -0,0 +1,10 @@
+use day09::part2::process;
+use miette::Context;
+
+fn main() -> miette::Result<()> {
+  tracing_subscriber::fmt::init();
+  let file = include_str!("../../input2.txt"); 
+  let result = process(file).context("process part2")?;
+  println!("{}", result);
+  Ok(())
+}
